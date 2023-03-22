47da44e565f11a6b494f00c09ce97881f6095786
Liza-Diata <elise.diata15@gmail.com>
2023-03-22 13:34:29 +0800

    add files

---
+++ budget_buddy_app/resources/views/faqs.blade.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')    
+</head>
+<body> 
+    <div class="text-center pt-20">
+    <h2 class="font-bold text-5xl mt-5 tracking-tight">
+                    FAQ
+                </h2>
+<p class="text-neutral-500 text-xl mt-3">
+                    Frequenty asked questions
+                </p>
+    </div>
+
+        <div class="max-w-screen-xl mx-auto px-5 pt-10 min-h-sceen flex">
+        <img class="h-96 ml-20" src="{{ URL('images/Thinking.png') }}" alt="">
+            <div class="flex flex-col items-center">
+               
+               
+            </div>
+            <div class="grid divide-y divide-neutral-200 max-w-xl mx-auto mt-8 bg-purple-900 rounded-lg text-gray-300 p-5">
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span >What is a budget tracking app?</span>
+                            <span class="transition group-open:rotate-180">
+                        <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+        </svg>
+                    </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn bg-indigo-100 p-5 rounded-xl">
+                        A budget tracking app is a software application that helps users monitor and manage their finances. It allows users to input their income and expenses, set budgets, track spending, and analyze financial data.
+
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span> How does a budget tracking app work?</span>
+                            <span class="transition group-open:rotate-180">
+                        <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+        </svg>
+                    </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn bg-indigo-100 p-5 rounded-xl">
+                        Budget tracking apps work by allowing users to input their financial data, including income and expenses. The app then uses this data to track spending, set budgets, and generate reports and analytics. Users can also set alerts and reminders for bill payments and other financial deadlines.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span> Are budget tracking apps safe and secure?</span>
+                            <span class="transition group-open:rotate-180">
+                        <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+        </svg>
+                    </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn bg-indigo-100 p-5 rounded-xl">
+                        Most budget tracking apps use encryption and other security measures to protect users' financial data. However, it's important to do your research and choose a reputable app with a good track record for security.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span> How much does a budget tracking app cost?</span>
+                            <span class="transition group-open:rotate-180">
+                        <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+        </svg>
+                    </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn bg-indigo-100 p-5 rounded-xl">
+                        The cost of budget tracking apps varies widely. Some are free, while others require a subscription or one-time payment. It's important to consider the features and functionality of the app when evaluating its cost.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span> Can budget tracking apps help me save money?</span>
+                            <span class="transition group-open:rotate-180">
+                        <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+        </svg>
+                    </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn bg-indigo-100 p-5 rounded-xl">
+                        Yes, budget tracking apps can help users save money by providing insights into their spending habits and allowing them to set and track financial goals. By identifying areas where they can cut back on spending and setting achievable financial goals, users can improve their financial health and increase their savings.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span> What are some features of a good budget tracking app?</span>
+                            <span class="transition group-open:rotate-180">
+                        <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+        </svg>
+                    </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn bg-indigo-100 p-5 rounded-xl">
+                        Some features of a good budget tracking app include the ability to track income and expenses, set and monitor budgets, generate reports and analytics, and set alerts and reminders for bill payments and other financial deadlines. Other useful features might include the ability to link bank and credit card accounts, track investment portfolios, and monitor credit scores.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span>Can I use a budget tracking app on multiple devices?</span>
+                            <span class="transition group-open:rotate-180">
+                        <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+        </svg>
+                    </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn bg-indigo-100 p-5 rounded-xl">
+                        Most budget tracking apps can be used on multiple devices, including smartphones, tablets, and desktop computers. However, it's important to check the app's compatibility with different operating systems and devices before downloading it.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span> How do I choose the right budget tracking app for me?</span>
+                            <span class="transition group-open:rotate-180">
+                        <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+        </svg>
+                    </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn bg-indigo-100 p-5 rounded-xl">
+                        When choosing a budget tracking app, it's important to consider your specific needs and goals. Look for an app that offers the features and functionality you need, as well as one that fits your budget and is compatible with your devices. You may also want to read reviews and compare multiple apps before making a decision.
+                        </p>
+                    </details>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
