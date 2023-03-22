54b571f14e84ab1875f7547aa57def64c34b2f25
Liza-Diata <elise.diata15@gmail.com>
2023-03-22 16:11:05 +0800

    modify files

--- budget_buddy_app/resources/views/about.blade.php
+++ budget_buddy_app/resources/views/about.blade.php
@@ -13,8 +13,8 @@
             <p class="p-10 text-2xl font-semibold text-white">ABOUT US</p>
         </div>  
         <div class="mx-auto grid max-w-4xl grid-cols-12 gap-4 p-5 px-6"> 
-            <div class="col-span-3 pt-5 text-justify sm:col-span-8"> 
-                <p>Welcome to Budget-Buddy, the budget tracking app designed to help you manage your finances and achieve your financial goals.</p><br/>
+            <div class="col-span-3 pt-5 text-justify sm:col-span-8 font-semibold"> 
+                <p>Welcome to Budget Buddy, the budget tracking app designed to help you manage your finances and achieve your financial goals.</p><br/>
                 <p>At Budget-Buddy, we believe that financial management should be simple, straightforward, and accessible to everyone. That's why we've created an app that makes it easy to track your income and expenses, set budgets, and plan for the future.</p><br/>
                 <p>Our app is designed with you in mind. Whether you're just starting out on your financial journey or you're an experienced budgeter, Budget-Buddy has everything you need to stay on top of your finances.</p>           
             </div>
@@ -22,29 +22,29 @@
                 <img class="h-80 ml-6" src="{{ URL('images/ManageMoney-bro.png') }}" alt="">
             </div>
             <div class="col-span-12 pt-10">
-                <p class="text-2xl  font-semibold">With <span class="text-purple-700 font-bold">BUDGET-BUDDY</span>, you can:</p>
+                <p class="text-2xl  font-semibold">With <span class="text-purple-700 font-bold">BUDGET BUDDY</span>, you can:</p>
             </div>
         </div>
-        <div class="mx-auto pl-48 grid grid-cols-12 gap-2">
+        <div class="mx-auto pl-48 grid grid-cols-12 gap-2 sm-cols-2">
             <div class="col-span-2 rounded-lg border p-2 h-60 flex flex-col shadow-lg hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-3 active:translate-y-0">
             <img class="h-36" src="{{ URL('images/Spreadsheets-bro.png') }}" alt="">
-                <p class="text-sm">Track your income and expenses in real-time</p>
+                <p class="text-sm font-semibold">Track your income and expenses in real-time</p>
             </div> 
             <div class="col-span-2 rounded-lg border p-2 h-60 flex flex-col shadow-lg hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-3 active:translate-y-0">
             <img class="h-36" src="{{ URL('images/Financialdata-bro.png') }}" alt="">
-                <p class="text-sm">Set personalized budgets for different categories of expenses</p>
+                <p class="text-sm font-semibold">Set personalized budgets for different categories of expenses</p>
             </div> 
             <div class="col-span-2 rounded-lg border p-2 h-60 flex flex-col shadow-lg hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-3 active:translate-y-0">
             <img class="h-36" src="{{ URL('images/PersonalBudget.png') }}" alt="">
-                <p class="text-sm">Plan for the future with savings goals and debt reduction plans</p>
+                <p class="text-sm font-semibold">Plan for the future with savings goals and debt reduction plans</p>
             </div> 
             <div class="col-span-2 rounded-lg border p-2 h-60 flex flex-col shadow-lg hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-3 active:translate-y-0">
             <img class="h-36" src="{{ URL('images/Bankruptcy-cuate.png') }}" alt="">
-                <p class="text-sm">Receive alerts when you're approaching or exceeding your budget limits</p>
+                <p class="text-sm font-semibold">Receive alerts when you're approaching or exceeding your budget limits</p>
             </div> 
             <div class="col-span-2 rounded-lg border p-2 h-60 flex flex-col shadow-lg hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-3 active:translate-y-0">
             <img class="h-36" src="{{ URL('images/Finance-cuate.png') }}" alt="">
-                <p class="text-sm">See your spending patterns and identify areas where you can save money</p>
+                <p class="text-sm font-semibold">See your spending patterns and identify areas where you can save money</p>
             </div> 
         </div>            
     </div>
--- budget_buddy_app/resources/views/landingpage.blade.php
+++ budget_buddy_app/resources/views/landingpage.blade.php
@@ -5,7 +5,14 @@
         <div class="col-span-4 md:col-span-6 pt-20">
             <p class="font-bold text-4xl">Say <span class="text-white text-5xl font-extrabold">GOODBYE</span> to <span class="text-violet-900">FINANCIAL STRESS</span> and <span class="text-white text-5xl font-extrabold">HELLO</span> to <span class="text-violet-900">FINANCIAL FREEDOM</span> </p>
             <p class="pt-10 text-xl font-bold"> Get 30 days FREE ACCESS and get your finances back on track today.</p>
-            <button class="text-white bg-[#21083b] p-2 font-semibold rounded-lg">GET STARTED</button>
+            <a href="dashboard">
+            <button class="btn group flex items-center bg-transparent pt-2 text-xl font-extrabold tracking-widest text-white">
+                <span class="relative pr-4 pb-1 text-wihte after:transition-transform after:duration-500 after:ease-out after:absolute after:bottom-0 after:left-0 after:block after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-violet-900 after:content-[''] after:group-hover:origin-bottom-left after:group-hover:scale-x-100">Get Started</span></a>
+                <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal" class="-translate-x-2 fill-slate-700 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:scale-x-105 group-hover:fill-white">
+                <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
+                </svg>
+  </button>
+
 
         </div>
     </section>
